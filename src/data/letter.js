eb6642cbfb3c87947a957e098fdbf261017174b6
gitucha <briantablet23@gmail.com>
2026-07-07 15:39:03 +0300

    naah this letter man

--- src/data/letter.js
+++ src/data/letter.js
@@ -10,6 +10,7 @@ const letter = {
     "This year, I've watched you handle things I don't think you give yourself credit for, and somehow still show up funny, warm, and completely yourself.",
     "I hope this year gives you exactly what you need, and I hope you know how loved you are — today and every ordinary day after it.",
     "I want you to be yourself,primarily always and i want to give you the stage to do so coz i have seen you in that light and its beautiful, pleasant, thrilling and it motivates me to be myself as well and I remain a happy man that i get witness it and i cant wait for the day we get to be around one another for as long as possible, i want to do everything with you, build,play,sing,create,dance,cook,date,make and mate, whatever it is, i think i would be very satisfied if it was just you, 'you make me chikwere😂'",
+    "'You my Baby Gurl fr🫶'",
     "I know things might not be as we might've wanted right now but you will always have a soft spot in my heart and i hope we can work things out and find our way back to each others arms.",
     "** I'm probably send this much later than i should've"
   ],
